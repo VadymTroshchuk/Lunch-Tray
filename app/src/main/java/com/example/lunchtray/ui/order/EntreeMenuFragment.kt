7cf40e70a57c628c552edd316db70b6736800d93
Vadym Troshchuk <vadimtrosuk01@gmail.com>
2023-11-15 11:26:39 +0200

    implement the setOnClickListener() to navigate to the EntreeMenuFragment

--- app/src/main/java/com/example/lunchtray/ui/order/EntreeMenuFragment.kt
+++ app/src/main/java/com/example/lunchtray/ui/order/EntreeMenuFragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.lunchtray.R
 import com.example.lunchtray.databinding.FragmentEntreeMenuBinding
 import com.example.lunchtray.model.OrderViewModel
 
@@ -55,6 +57,8 @@ class EntreeMenuFragment : Fragment() {
      */
     fun goToNextScreen() {
         // TODO: Navigate to the SideMenuFragment
+        findNavController()
+            .navigate(R.id.action_entreeMenuFragment_to_sideMenuFragment)
     }
 
     /**
@@ -63,6 +67,8 @@ class EntreeMenuFragment : Fragment() {
     fun cancelOrder() {
         // TODO: Reset order in view model
         // TODO: Navigate back to the [StartFragment] to start over
+        findNavController()
+            .navigate(R.id.action_entreeMenuFragment_to_startOrderFragment)
     }
 
     /**
--- app/src/main/java/com/example/lunchtray/ui/order/StartOrderFragment.kt
+++ app/src/main/java/com/example/lunchtray/ui/order/StartOrderFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.lunchtray.R
 import com.example.lunchtray.databinding.FragmentStartOrderBinding
 
 /**
@@ -32,6 +34,7 @@ class StartOrderFragment : Fragment() {
         // Navigate to entree menu
         binding.startOrderBtn.setOnClickListener {
             // TODO: navigate to the EntreeMenuFragment
+            findNavController().navigate(R.id.action_startOrderFragment_to_entreeMenuFragment)
         }
         return root
     }
